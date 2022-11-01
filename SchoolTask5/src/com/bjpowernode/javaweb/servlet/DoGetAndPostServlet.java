eb1c8654eb9463e7cdc223cdd02252257bc8266d
Michael-Su <Bin20020102@outlook.com>
2022-11-01 19:39:19 +0800

    IDEA STask

---
+++ SchoolTask5/src/com/bjpowernode/javaweb/servlet/DoGetAndPostServlet.java
@@ -0,0 +1,26 @@
+package com.bjpowernode.javaweb.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * @author Sujunbin
+ * @create 2022/10/31 13:01
+ */
+public class DoGetAndPostServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("this is doGet Method,the servlet is created by IntelliJ IDEA");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("this is doPost Method");
+
+    }
+}
